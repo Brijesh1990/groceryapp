30ecf6d0f5545dde40ac0e8bc6739ad39a995902
Brijesh1990 <bkpandey.pandey@gmail.com>
2025-04-05 14:53:50 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,14 @@ import AddCategory from './components/admin/AddCategory'
 import DeleteCategory from './components/admin/DeleteCategory'
 import EditCategory from './components/admin/EditCategory'
 import AddItems from './components/admin/AddItems'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+// import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { HashRouter as Router,Routes,Route } from 'react-router-dom'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
     <Routes>
-      <Route path='https://brijesh1990.github.io/groceryapp/' element={<Layout />}></Route>
+      <Route path='/' element={<Layout />}></Route>
       <Route path='/admin-login' element={<Login />}></Route>
       <Route path='/admin-login/dashboard' element={<AdminLayout />}></Route>
       <Route path='/admin-login/add-category' element={<AddCategory />}></Route>
